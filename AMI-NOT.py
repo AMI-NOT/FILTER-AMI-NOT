8dd67af85ce151c9948284070a92c4b7971e883c
AMI-NOT <122160679+AMI-NOT@users.noreply.github.com>
2023-01-08 22:57:55 +0330

    Add files via upload

---
+++ AMI-NOT.py
@@ -0,0 +1,66 @@
+
+
+password = input("enter youre pass : ")
+while password != ("AMI-NOT-79"):
+	print("password ×")
+	password = input("enter youre pass : ")
+print("pasword☆")
+
+#Noob Editors
+#Posy you mother Editor
+from colorama import init, Fore
+init()
+from random import choice, randint
+print(Fore.RED+'》》》》》》》》 FILTERING AMI-NOT《《《《《《《《')
+print(Fore.GREEN+'''
+                   _               _
+                  (_)             | |
+    __ _ _ __ ___  _   _ __   ___ | |_
+   / _` | '_ ` _ \| | | '_ \ / _ \| __|
+  | (_| | | | | | | | | | | | (_) | |_
+   \__,_|_| |_| |_|_| |_| |_|\___/ \__|''')
+ 
+smg = print(f'\33[92m    Version 2.9.5')
+cod = print(Fore.BLUE+' [a] Code Sazi')
+bug = print(Fore.RED+' [b] Bug list')
+xxx = print(Fore.CYAN+' [c] Link xxx or Site xxx list')
+virus = print(Fore.WHITE+' [d] virus liste')
+tur = print(Fore.YELLOW+' [h] Code Tor')
+server = print (Fore.BLUE+' [g] cod server')
+gl =input(f'\33[92m Enter number: ')
+if gl=='a':
+
+    bu = input(Fore.BLUE+' Enter Bug: ')
+    xx = input(Fore.BLUE+' Enter Link xxx: ')
+    tor = input(Fore.BLUE+' Enter code tor: ')
+    lok = input(Fore.BLUE+' Enter code shakhsi: ')
+    virus = input(Fore.BLUE+' Enter code virus:')
+    server = input(Fore.BLUE+'Enter code server')
+    amir =(bu+'FILTERIG'+server+'sex.sex.sex'+xx+'/'+tor+'))'+lok+'/sex-filter='+virus+'rubika-filter.py(AMI-NOT)filter')
+    print(amir)
+algoritm = ["/y//d/f/","/f//d/","/d//f/h/","/f//a/y/","/e////f.h/","/f/h.u//","/f////g.h/","/g//d/","/f/h.g//","/r//g/h"]
+alguritm = choice(algoritm)
+if gl == 'b':
+    print(f'(\{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{alguritm}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)})')
+if gl == 'c':
+    print(Fore.CYAN+' pornhub.com')
+    print('-----------')
+    print(Fore.CYAN+' xnxx.com')
+    print('-----------')
+    print(Fore.CYAN+' sexbebin.com')
+    print('-----------')
+    print(Fore.CYAN+' link sex ^')
+if gl == 'h':
+    print(Fore.YELLOW+''' new terminal code 
+    pkg install tor
+    tor''')
+if gl == 'd':
+    print(Fore.CYAN+' http://ffmodmenu.page.link/Anti-V')
+if gl == 'g':
+     print(Fore.CYAN+' yftt15k.in')
+     print('-----------')
+     print(Fore.CYAN+' yftt16k.in')
+     print('-----------')
+     print(Fore.CYAN+' sex-yftt14k.in')
+     print('-----------')
+    
