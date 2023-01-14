de6310a5470c0694e8b5158e285d8ad3d8b54446
AMI-NOT <122160679+AMI-NOT@users.noreply.github.com>
2023-01-14 18:34:15 +0330

    Add files via upload

---
+++ FILTER-AMI-NOT.py
@@ -0,0 +1,110 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Noob Editors
+#Posy you mother Editor
+from colorama import init, Fore
+init()
+from random import choice, randint
+
+print(Fore.CYAN+'------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------')
+print("                                            ")
+print(Fore.RED+'      》》》》》》》》 FILTERING AMI-NOT《《《《《《《《')
+print('                                                                      ')
+
+
+
+print(Fore.CYAN+'    coded by AMI-NOT')
+print(Fore.RED+'                   ●○●○●○●○●○●○●○●○●○●○●○●')
+print(Fore.RED+'                   ○                     ●')
+print(Fore.RED+'                   ●                     ○')
+print(Fore.RED+'                   ○                     ●')
+print(Fore.RED+'                   ●                     ○')
+print(Fore.GREEN+'                   》      AMI-NOT      《')
+print(Fore.RED+'                   ○                     ●')
+print(Fore.RED+'                   ●                     ○')
+print(Fore.RED+'                   ○                     ●')
+print(Fore.RED+'                   ●○●○●○●○●○●○●○●○●○●○●○●')
+print(Fore.GREEN+'■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□')
+print(Fore.YELLOW+'         telegram : Mr_AMI_NOT ')
+print(Fore.YELLOW+'         instagram : me_AMI.NOT')
+print(Fore.RED+'         GITHUB : https://github.com/AMI-NOT')
+print(Fore.GREEN+'■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□■□')
+
+print(Fore.CYAN+'           filter                                                 AMI-NOT   ')
+
+
+
+password = input("enter youre pass : ")
+while password != ("AMI-NOT-79"):
+	print(Fore.RED+'password ×')
+	password = input("enter youre pass : ")
+print(Fore.GREEN+'pasword☆')
+
+print('                                                                                                                                                                                                            ')
+
+print(Fore.CYAN+'        AMI-NOT')
+
+smg = print(f'\33[92m    Version 2.9.5')
+cod = print(Fore.BLUE+' [1] Code Sazi')
+bug = print(Fore.RED+' [2] Bug list')
+xxx = print(Fore.CYAN+' [3] Link xxx or Site xxx list')
+virus = print(Fore.WHITE+' [4] nano-not')
+nano = print(Fore.YELLOW+' [5] virus')
+nano2 = print (Fore.BLUE+' [6] nano sex')
+gl =input(f'\33[92m Enter number: ')
+if gl=='1':
+
+    bu = input(Fore.BLUE+' Enter Bug: ')
+    xx = input(Fore.BLUE+' Enter Link xxx: ')
+    nano = input(Fore.BLUE+' Enter nano-not: ')
+    lok = input(Fore.BLUE+' Enter code shakhsi: ')
+    virus = input(Fore.BLUE+' Enter code virus:')
+    nano2 = input(Fore.BLUE+' Enter nano sex:')
+    amir =(nano+'(FILTERIG)'+nano2+')sex.sex.sex='+xx+'{filter}'+lok+'/sex-filter='+virus+'rubika-filter.py('+bu+')filter')
+    print('                                 ')
+    print(Fore.CYAN+'     yuore code filter          {AMI-NOT}')   
+    print('                                                        ')
+    
+    print(amir)
+algoritm = ["/y//d/f/","/f//d/","/d//f/h/","/f//a/y/","/e////f.h/","/f/h.u//","/f////g.h/","/g//d/","/f/h.g//","/r//g/h"]
+alguritm = choice(algoritm)
+if gl == '2':
+    print(f'(\{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{alguritm}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)}.{randint(1,9)})')
+if gl == '3':
+    print(Fore.CYAN+' pornhub.com')
+    print('-------------------------------------------------------')
+    print(Fore.CYAN+' xnxx.com')
+    print('-------------------------------------------------------')
+    print(Fore.CYAN+' sexbebin.com')
+    print('-------------------------------------------------------')
+    print(Fore.CYAN+' link sex ^')
+if gl == '4':
+    print(Fore.YELLOW+' https://globalvision.com.vn/vantindat/images/Sex-report-AMI-NOT.html ')
+    print('------------------------------------------------------')    
+    print(Fore.YELLOW+' http://www.gaobrvt.com/vantindat/images/nano-not-filter-CNC-AMI-NOT-100sex.html ')
+if gl == '5':
+    print(Fore.CYAN+' http://ffmodmenu.page.link/Anti-V')
+    print('------------------------------------------------------')
+    print(Fore.CYAN+'https://dl.uploadgram.me/60bdffd3a2be4h(python' )  
+    print('-------------------------------------------------------')
+    print(Fore.CYAN+'https://dl.uploadgram.me/60bdffd3a2be4h')
+if gl == '6':
+     print(Fore.CYAN+' https://globalvision.com.vn/vantindat/images/Filtering-AMI-NOT(sex)hackers.html')
+     print('-------------------------------------------------------')
+if gl == '7':
+    pront(Fore.RED+'https://globalvision.com.vn/vantindat/images/Sex-report-AMI-NOT.html')
+    print("--------------------------------------------")
+    print(Fore.BLUE+'https://globalvision.com.vn/vantindat/images/Filtering-AMI-NOT(sex)hackers.html')
+    
